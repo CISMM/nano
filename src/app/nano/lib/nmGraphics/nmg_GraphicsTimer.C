0a627fd4b305904d81195efb84c0e59a51d5a8bd
weigle <>
2000-02-18 22:23:30 +0000

    A few adds here ...

---
+++ src/app/nano/lib/nmGraphics/nmg_GraphicsTimer.C
@@ -0,0 +1,502 @@
+#include  "nmg_GraphicsTimer.h"
+
+nmg_Graphics_Timer::nmg_Graphics_Timer (nmg_Graphics * imp,
+                                        nmb_TimerList * timer) :
+  nmg_Graphics (NULL, ""),
+  d_imp (imp),
+  d_timer (timer) {
+
+}
+
+nmg_Graphics_Timer::~nmg_Graphics_Timer (void) {
+
+}
+
+
+// PROTECTED
+
+void nmg_Graphics_Timer::activateTimer (void) {
+  if (d_timer) {
+    d_timer->activate(d_timer->getListHead());
+  }
+}
+
+
+
+
+
+void nmg_Graphics_Timer::mainloop (void) {
+
+  d_imp->mainloop();
+  
+}
+
+void nmg_Graphics_Timer::resizeViewport (int width, int height) {
+  activateTimer();
+  d_imp->resizeViewport(width, height);
+}
+
+void nmg_Graphics_Timer::getDisplayPosition (q_vec_type &ll,
+        q_vec_type &ul, q_vec_type &ur)
+{
+  activateTimer();
+  d_imp->getDisplayPosition(ll, ul, ur);
+}
+
+void nmg_Graphics_Timer::loadRulergridImage (const char * name) {
+  activateTimer();
+  d_imp->loadRulergridImage(name);
+}
+
+
+void nmg_Graphics_Timer::causeGridRedraw (void) {
+  activateTimer();
+  d_imp->causeGridRedraw();
+}
+
+void nmg_Graphics_Timer::causeGridRebuild (void) {
+  activateTimer();
+  d_imp->causeGridRebuild();
+}
+
+void nmg_Graphics_Timer::enableChartjunk (int on) {
+  activateTimer();
+  d_imp->enableChartjunk(on);
+}
+
+void nmg_Graphics_Timer::enableFilledPolygons (int on) {
+  activateTimer();
+  d_imp->enableFilledPolygons(on);
+}
+
+void nmg_Graphics_Timer::enableSmoothShading (int on) {
+  activateTimer();
+  d_imp->enableSmoothShading(on);
+}
+
+void nmg_Graphics_Timer::enableTrueTip (int on) {
+  activateTimer();
+  d_imp->enableTrueTip(on);
+}
+
+
+
+
+void nmg_Graphics_Timer::setAdhesionSliderRange (float low,
+                                                          float high) {
+  activateTimer();
+  d_imp->setAdhesionSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setAlphaColor (float r, float g, float b) {
+  activateTimer();
+  d_imp->setAlphaColor(r,g,b);
+}
+
+void nmg_Graphics_Timer::setAlphaSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setAlphaSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setBumpMapName (const char * name) {
+  activateTimer();
+  d_imp->setBumpMapName(name);
+}
+
+void nmg_Graphics_Timer::setColorMapDirectory (const char * dir) {
+  activateTimer();
+  d_imp->setColorMapDirectory(dir);
+}
+
+void nmg_Graphics_Timer::setTextureDirectory (const char * dir) {
+  activateTimer();
+  d_imp->setTextureDirectory(dir);
+}
+
+void nmg_Graphics_Timer::setColorMapName (const char * name) {
+  activateTimer();
+  d_imp->setColorMapName(name);
+}
+
+void nmg_Graphics_Timer::setColorSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setColorSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setComplianceSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setComplianceSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setContourColor (int r, int g, int b) {
+  activateTimer();
+  d_imp->setContourColor(r,g,b);
+}
+
+void nmg_Graphics_Timer::setFrictionSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setFrictionSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setBumpSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setBumpSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setBuzzSliderRange (float low, float high) {
+  activateTimer();
+  d_imp->setBuzzSliderRange(low, high);
+}
+
+void nmg_Graphics_Timer::setHandColor (int c) {
+  activateTimer();
+  d_imp->setHandColor(c);
+}
+
+void nmg_Graphics_Timer::setIconScale (float scale) {
+  activateTimer();
+  d_imp->setIconScale(scale);
+}
+
+void nmg_Graphics_Timer::setCollabHandPos(double pos[], double quat[])
+{
+  activateTimer();
+  d_imp->setCollabHandPos(pos, quat);
+}
+
+void nmg_Graphics_Timer::setCollabMode(int mode)
+{
+  activateTimer();
+  d_imp->setCollabMode(mode);
+}
+
+void nmg_Graphics_Timer::setHatchMapName (const char * name) {
+  activateTimer();
+  d_imp->setHatchMapName(name);
+}
+
+
+void nmg_Graphics_Timer::setAlphaPlaneName (const char * n) {
+  activateTimer();
+  d_imp->setAlphaPlaneName(n);
+}
+
+void nmg_Graphics_Timer::setColorPlaneName (const char * n) {
+  activateTimer();
+  d_imp->setColorPlaneName(n);
+}
+
+void nmg_Graphics_Timer::setContourPlaneName (const char * n) {
+  activateTimer();
+  d_imp->setContourPlaneName(n);
+}
+
+void nmg_Graphics_Timer::setHeightPlaneName (const char * n) {
+  activateTimer();
+  d_imp->setHeightPlaneName(n);
+}
+
+
+void nmg_Graphics_Timer::setContourWidth (float x) {
+  activateTimer();
+  d_imp->setContourWidth(x);
+}
+
+void nmg_Graphics_Timer::setMinColor (const double c [4]) {
+  activateTimer();
+  d_imp->setMinColor(c);
+}
+
+void nmg_Graphics_Timer::setMaxColor (const double c [4]) {
+  activateTimer();
+  d_imp->setMaxColor(c);
+}
+
+void nmg_Graphics_Timer::setMinColor (const int c [4]) {
+  activateTimer();
+  d_imp->setMinColor(c);
+}
+
+void nmg_Graphics_Timer::setMaxColor (const int c [4]) {
+  activateTimer();
+  d_imp->setMaxColor(c);
+}
+
+void nmg_Graphics_Timer::setPatternMapName (const char * name) {
+  activateTimer();
+  d_imp->setPatternMapName(name);
+}
+
+void nmg_Graphics_Timer::sendGeneticTexturesData (
+                           int number_of_variables,
+                           char **variable_list ) {
+  activateTimer();
+  d_imp->sendGeneticTexturesData(number_of_variables, variable_list);
+}
+
+void nmg_Graphics_Timer::createRealignTextures( const char *name ) {
+  activateTimer();
+  d_imp->createRealignTextures(name);
+}
+
+void nmg_Graphics_Timer::
+setRealignTexturesConversionMap( const char *map, const char *mapdir ) {
+  activateTimer();
+  d_imp->setRealignTexturesConversionMap(map, mapdir);
+}
+
+void nmg_Graphics_Timer::setRealignTextureSliderRange (float low,
+								float high) {
+  activateTimer();
+  d_imp->setRealignTextureSliderRange(low, high);
+}
+
+
+void nmg_Graphics_Timer::computeRealignPlane( const char *name,
+						       const char *newname ) {
+  activateTimer();
+  d_imp->computeRealignPlane(name, newname);
+}
+
+void nmg_Graphics_Timer::translateTextures ( int on,
+						      float dx, float dy ) {
+  activateTimer();
+  d_imp->translateTextures(on, dx, dy);
+}
+
+void nmg_Graphics_Timer::scaleTextures ( int on,
+						  float dx, float dy ) {
+  activateTimer();
+  d_imp->scaleTextures(on, dx, dy);
+}
+
+void nmg_Graphics_Timer::shearTextures ( int on,
+						  float dx, float dy ) {
+  activateTimer();
+  d_imp->shearTextures(on, dx, dy);
+}
+
+void nmg_Graphics_Timer::rotateTextures ( int on, float theta ) {
+  activateTimer();
+  d_imp->rotateTextures(on, theta);
+}
+
+void nmg_Graphics_Timer::setTextureCenter( float dx, float dy ) {
+  activateTimer();
+  d_imp->setTextureCenter(dx, dy);
+}
+
+void nmg_Graphics_Timer::loadRawDataTexture(const int which,
+       const char *image_name,
+       const int start_x, const int start_y)
+{
+  activateTimer();
+  d_imp->loadRawDataTexture(which, image_name, start_x, start_y);
+}
+
+void nmg_Graphics_Timer::updateTexture(int which_texture,
+       const char *image_name,
+       int start_x, int start_y,
+       int end_x, int end_y)
+{
+  activateTimer();
+  d_imp->updateTexture(which_texture, image_name, start_x, start_y,
+                       end_x, end_y);
+}
+
+void nmg_Graphics_Timer::setTextureTransform(double *xform){
+  activateTimer();
+  d_imp->setTextureTransform(xform);
+}
+
+
+void nmg_Graphics_Timer::setRulergridAngle (float v) {
+  activateTimer();
+  d_imp->setRulergridAngle(v);
+}
+
+void nmg_Graphics_Timer::setRulergridColor (int r, int g, int b) {
+  activateTimer();
+  d_imp->setRulergridColor(r, g, b);
+}
+
+void nmg_Graphics_Timer::setRulergridOffset (float x, float y) {
+  activateTimer();
+  d_imp->setRulergridOffset(x, y);
+}
+
+void nmg_Graphics_Timer::setRulergridOpacity (float alpha) {
+  activateTimer();
+  d_imp->setRulergridOpacity(alpha);
+}
+
+void nmg_Graphics_Timer::setRulergridScale (float s) {
+  activateTimer();
+  d_imp->setRulergridScale(s);
+}
+
+void nmg_Graphics_Timer::setRulergridWidths (float x, float y) {
+  activateTimer();
+  d_imp->setRulergridWidths(x, y);
+}
+
+void nmg_Graphics_Timer::setSpecularity (int s) {
+  activateTimer();
+  d_imp->setSpecularity(s);
+}
+
+
+void nmg_Graphics_Timer::setDiffusePercent (float d) {
+  activateTimer();
+  d_imp->setDiffusePercent(d);
+}
+
+void nmg_Graphics_Timer::setSurfaceAlpha (float a) {
+  activateTimer();
+  d_imp->setSurfaceAlpha(a);
+}
+
+void nmg_Graphics_Timer::setSpecularColor (float s) {
+  activateTimer();
+  d_imp->setSpecularColor(s);
+}
+
+void nmg_Graphics_Timer::setSphereScale (float s) {
+  activateTimer();
+  d_imp->setSphereScale(s);
+}
+
+void nmg_Graphics_Timer::setTesselationStride (int s) {
+  activateTimer();
+  d_imp->setTesselationStride(s);
+}
+
+void nmg_Graphics_Timer::setTextureMode (TextureMode m,
+	TextureTransformMode xm) {
+  activateTimer();
+  d_imp->setTextureMode(m, xm);
+}
+
+void nmg_Graphics_Timer::setTextureScale (float f) {
+  activateTimer();
+  d_imp->setTextureScale(f);
+}
+
+void nmg_Graphics_Timer::setTrueTipScale (float f) {
+  activateTimer();
+  d_imp->setTrueTipScale(f);
+}
+
+
+void nmg_Graphics_Timer::setUserMode (int oldMode, int newMode,
+                                               int style) {
+  activateTimer();
+  d_imp->setUserMode(oldMode, newMode, style);
+}
+
+
+void nmg_Graphics_Timer::setLightDirection (q_vec_type & v) {
+  activateTimer();
+  d_imp->setLightDirection(v);
+}
+
+void nmg_Graphics_Timer::resetLightDirection (void) {
+  activateTimer();
+  d_imp->resetLightDirection();
+}
+
+
+int nmg_Graphics_Timer::addPolylinePoint (const float point [2][3]) {
+  activateTimer();
+  d_imp->addPolylinePoint(point);
+}
+
+void nmg_Graphics_Timer::emptyPolyline (void) {
+  activateTimer();
+  d_imp->emptyPolyline();
+}
+
+void nmg_Graphics_Timer::setRubberLineStart (float p0, float p1) {
+  activateTimer();
+  d_imp->setRubberLineStart(p0, p1);
+}
+
+void nmg_Graphics_Timer::setRubberLineEnd (float p2, float p3 ) {
+  activateTimer();
+  d_imp->setRubberLineEnd(p2, p3);
+}
+
+void nmg_Graphics_Timer::setRubberLineStart (const float p [2]) {
+  activateTimer();
+  d_imp->setRubberLineStart(p);
+}
+
+void nmg_Graphics_Timer::setRubberLineEnd (const float p [2]) {
+  activateTimer();
+  d_imp->setRubberLineEnd(p);
+}
+
+
+void nmg_Graphics_Timer::setScanlineEndpoints(const float p0[3],
+		const float p1[3]){
+  activateTimer();
+  d_imp->setScanlineEndpoints(p0, p1);
+}
+
+void nmg_Graphics_Timer::displayScanlinePosition(const int enable)
+{
+  activateTimer();
+  d_imp->displayScanlinePosition(enable);
+}
+
+void nmg_Graphics_Timer::positionAimLine (const PointType top,
+                                                   const PointType bottom) {
+  activateTimer();
+  d_imp->positionAimLine(top, bottom);
+}
+
+void nmg_Graphics_Timer::positionRubberCorner
+    (float minx, float miny, float maxx, float maxy) {
+  activateTimer();
+  d_imp->positionRubberCorner(minx, miny, maxx, maxy);
+}
+
+void nmg_Graphics_Timer::positionSweepLine (const PointType top,
+                                                     const PointType bottom) {
+  activateTimer();
+  d_imp->positionSweepLine(top, bottom);
+}
+
+void nmg_Graphics_Timer::positionSphere (float x, float y, float z) {
+  activateTimer();
+  d_imp->positionSphere(x, y, z);
+}
+
+
+
+void nmg_Graphics_Timer::createScreenImage
+  (const char * filename, const ImageType type)
+{
+  activateTimer();
+  d_imp->createScreenImage(filename, type);
+}
+
+
+
+void nmg_Graphics_Timer::getLightDirection (q_vec_type * v) const {
+  d_imp->getLightDirection(v);
+}
+int nmg_Graphics_Timer::getHandColor (void) const {
+  return d_imp->getHandColor();
+}
+int nmg_Graphics_Timer::getSpecularity (void) const {
+  return d_imp->getSpecularity();
+}
+const double * nmg_Graphics_Timer::getMinColor (void) const {
+  return d_imp->getMinColor();
+}
+const double * nmg_Graphics_Timer::getMaxColor (void) const {
+  return d_imp->getMaxColor();
+}
+
+
---
+++ src/app/nano/lib/nmGraphics/nmg_GraphicsTimer.h
@@ -0,0 +1,147 @@
+#ifndef NMG_GRAPHICS_TIMER_H
+#define NMG_GRAPHICS_TIMER_H
+
+#include <nmb_TimerList.h>
+
+#include "nmg_Graphics.h"
+class nmg_Graphics_Implementation;  // from "nmg_GraphicsImpl.h"
+
+// Wish there was a cleaner way to do this -
+// We want to mark as active the current record in an nmb_TimerList
+// before executing ANY graphics call.  We could either search through
+// the rest of the code & explicitly activate it before making the call
+// into nmg_Graphics classes, or we can use subclassing and composition
+// to build classes that do that automatically and choose or not choose
+// them at runtime.
+
+class nmg_Graphics_Timer : public nmg_Graphics {
+
+  public:
+
+    nmg_Graphics_Timer (nmg_Graphics *, nmb_TimerList *);
+
+    virtual ~nmg_Graphics_Timer (void);
+
+  protected:
+
+    nmg_Graphics * d_imp;
+
+    void activateTimer (void);
+
+    nmb_TimerList * d_timer;
+
+  public:
+
+    virtual void mainloop (void);
+
+    // MANIPULATORS
+
+    virtual void resizeViewport(int width, int height);
+    virtual void getDisplayPosition (q_vec_type &ll, q_vec_type &ul,
+                                                     q_vec_type &ur);
+    virtual void loadRulergridImage (const char * name);
+    virtual void causeGridRedraw (void);
+    virtual void causeGridRebuild (void);
+    virtual void enableChartjunk (int on);
+    virtual void enableFilledPolygons (int on);
+    virtual void enableSmoothShading (int on);
+    virtual void enableTrueTip (int on);
+    virtual void setAdhesionSliderRange (float low, float hi);
+    virtual void setAlphaColor (float r, float g, float b);
+    virtual void setAlphaSliderRange (float low, float hi);
+    virtual void setBumpMapName (const char *);
+    virtual void setColorMapDirectory (const char *);
+    virtual void setColorMapName (const char *);
+    virtual void setColorSliderRange (float low, float hi);
+    virtual void setTextureDirectory (const char *);
+    virtual void setComplianceSliderRange (float low, float hi);
+    virtual void setContourColor (int r, int g, int b);
+    virtual void setContourWidth (float);
+    virtual void setFrictionSliderRange (float low, float hi);
+    virtual void setBumpSliderRange (float low, float hi);
+    virtual void setBuzzSliderRange (float low, float hi);
+    virtual void setHandColor (int);
+    virtual void setHatchMapName (const char *);
+    virtual void setAlphaPlaneName (const char *);
+    virtual void setColorPlaneName (const char *);
+    virtual void setContourPlaneName (const char *);
+    virtual void setHeightPlaneName (const char *);
+    virtual void setIconScale (float);
+    virtual void setCollabHandPos(double [3], double [4]);
+    virtual void setCollabMode(int);
+    virtual void setMinColor (const double [3]);
+    virtual void setMaxColor (const double [3]);
+    virtual void setMinColor (const int [3]);
+    virtual void setMaxColor (const int [3]);
+    virtual void setPatternMapName (const char *);
+    virtual void sendGeneticTexturesData (int, char **);
+    virtual void createRealignTextures( const char * );
+    virtual void setRealignTextureSliderRange (float, float);
+    virtual void setRealignTexturesConversionMap
+                    (const char *, const char *);
+    virtual void computeRealignPlane( const char *, const char * );
+    virtual void translateTextures ( int on, float dx, float dy );
+    virtual void scaleTextures ( int on, float dx, float dy );
+    virtual void shearTextures ( int on, float dx, float dy );
+    virtual void rotateTextures ( int on, float theta );
+    virtual void setTextureCenter( float dx, float dy );
+    virtual void loadRawDataTexture(const int which, const char *image_name,
+        const int start_x, const int start_y);
+    virtual void updateTexture(int which, const char *image_name,
+       int start_x, int start_y, 
+       int end_x, int end_y);
+    virtual void setTextureTransform(double *xform);
+    virtual void setRulergridAngle (float);
+    virtual void setRulergridColor (int r, int g, int b);
+    virtual void setRulergridOffset (float x, float y);
+    virtual void setRulergridOpacity (float alpha);
+    virtual void setRulergridScale (float);
+    virtual void setRulergridWidths (float x, float y);
+    virtual void setSpecularity (int);
+    virtual void setSpecularColor (float);
+    virtual void setDiffusePercent (float);
+    virtual void setSurfaceAlpha (float);
+    virtual void setSphereScale (float);
+    virtual void setTesselationStride (int);
+    virtual void setTextureMode (TextureMode,TextureTransformMode);
+    virtual void setTextureScale (float);
+    virtual void setTrueTipScale (float);
+    virtual void setUserMode (int oldMode, int newMode, int style);
+    virtual void setLightDirection (q_vec_type &);
+    virtual void resetLightDirection (void);
+    virtual int addPolylinePoint (const float [2][3]);
+    virtual void emptyPolyline (void);
+    virtual void setRubberLineStart (float, float);
+    virtual void setRubberLineEnd (float, float);
+    virtual void setRubberLineStart (const float [2]);
+    virtual void setRubberLineEnd (const float [2]);
+    virtual void setScanlineEndpoints(const float[3], const float [3]);
+    virtual void displayScanlinePosition(const int enable);
+    virtual void positionAimLine (const PointType top,
+                                  const PointType bottom);
+    virtual void positionRubberCorner (float x0, float y0,
+                                       float x1, float y1);
+    virtual void positionSweepLine (const PointType top,
+                                    const PointType bottom);
+    virtual void positionSphere (float x, float y, float z);
+    virtual void createScreenImage (const char * filename,
+                                    const ImageType type);
+
+
+    virtual void getLightDirection (q_vec_type *) const;
+    virtual int getHandColor (void) const;
+    virtual int getSpecularity (void) const;
+    virtual const double * getMinColor (void) const;
+    virtual const double * getMaxColor (void) const;
+
+};
+
+
+
+#endif  // NMG_GRAPHICS_TIMER_H
+
+
+
+
+
+
