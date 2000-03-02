ab4e509f3279913be3750356866d6bbf7ca87f55
weigle <>
2000-03-02 17:05:38 +0000

    New dir (missed the add 'cause I did it under cygwin, oops). Robot control library.

---
+++ src/app/nano/lib/nmRobotControl/RobotControl.C
@@ -0,0 +1,127 @@
+#include "RobotControl.h"
+
+#include "microscape.h"
+
+#include "nmb_Image.h"
+
+#include "vrpn_Types.h"
+
+#include "Point.h"
+
+ImageViewer *g_iViewer(NULL);
+
+int g_iViewerWin(-1);
+
+bool g_activeControl(false),
+     g_displayPlan(true);
+
+typedef enum { SCAN_MODE, IMAGE_MODE, MODIFY_MODE } ScopeMode;
+
+ScopeMode g_activeMode(SCAN_MODE);
+
+nmb_Dataset *g_dataset(NULL);
+
+nmm_Microscope_Remote *g_microscope(NULL);
+
+
+#ifndef V_GLUT
+
+#define GLUT_LEFT_BUTTON  0x0
+#define GLUT_RIGHT_BUTTON 0x1
+#define GLUT_UP           0x0
+#define GLUT_DOWN         0x1
+
+#endif
+
+#include "warrencntsim.C"
+
+
+int RobotControl::eventHandler(const ImageViewerWindowEvent &event, void *ud)
+{
+   switch(event.type)
+   {
+      case RESIZE_EVENT:
+         reshapeWindowFuncMain(event.width, event.height);
+         break;
+
+      case BUTTON_PRESS_EVENT:
+         if (g_displayPlan)
+            if (event.button == IV_LEFT_BUTTON)
+               mouseFuncMain(GLUT_LEFT_BUTTON, GLUT_DOWN, event.mouse_x, event.mouse_y);
+         break;
+
+      case MOTION_EVENT:
+         if (g_displayPlan)
+            if (event.state & IV_LEFT_BUTTON_MASK)
+               mouseMotionFuncMain(event.mouse_x, event.mouse_y);
+         break;
+
+      case KEY_PRESS_EVENT:
+         commonKeyboardFunc(event.keycode, event.mouse_x, event.mouse_y);
+         break;
+
+      default:
+         break;
+   }
+
+   return 0;
+}
+
+int RobotControl::displayHandler(const ImageViewerDisplayData &data, void *ud)
+{
+   if (g_displayPlan)
+      displayFuncMain();
+   else
+      displayFuncView();
+   return 0;
+}
+
+RobotControl::RobotControl(nmm_Microscope_Remote *microscope, nmb_Dataset *dataset)
+{
+   g_microscope = microscope;
+   g_dataset = dataset;
+   g_iViewer = ImageViewer::getImageViewer();
+
+   char *display_name;
+   display_name = (char *)getenv("V_X_DISPLAY");
+   if (!display_name)
+      display_name = (char *)getenv("DISPLAY");
+   if (!display_name)
+      display_name = strdup("unix:0");
+   else
+      display_name = strdup(display_name);
+
+   g_iViewer->init(display_name);
+
+   g_iViewerWin = g_iViewer->createWindow(display_name, 0, 0, windowWidth, windowHeight, "Robot Control");
+
+   g_iViewer->setWindowEventHandler(g_iViewerWin, RobotControl::eventHandler, (void *)this);
+   g_iViewer->setWindowDisplayHandler(g_iViewerWin, RobotControl::displayHandler, (void *)this);
+
+   initObs();
+
+   updateLiveData();
+}
+
+void RobotControl::mainloop(void)
+{
+   if (g_iViewer)
+   {
+      g_iViewer->mainloop();
+
+      if (robotRunning || robotImgPhase || robotModPhase)
+         g_iViewer->dirtyWindow(g_iViewerWin);
+      else
+         updateLiveData();
+   }
+}
+
+void RobotControl::show(void)
+{
+   g_iViewer->showWindow(g_iViewerWin);
+}
+
+void RobotControl::hide(void)
+{
+   g_iViewer->hideWindow(g_iViewerWin);
+}
---
+++ src/app/nano/lib/nmRobotControl/vec2d.h
@@ -0,0 +1,72 @@
+/*==========================================================================
+ *
+ *  Copyright (C) 1997 Warren Robinett. All Rights Reserved.
+ *
+ *  File:       vec2d.h
+ *  Content:    header file for class Vec2d
+ *
+ *  
+ *  
+ *
+ *  See file skel.doc for documentation, including bibliography
+ *
+ ***************************************************************************/
+
+#ifndef VEC2D_INCLUDED
+#define VEC2D_INCLUDED
+
+#ifdef NMRC_LIB
+#include "imageViewer.h"
+#endif
+
+class Vec2d {
+public:
+	double x;
+	double y;
+
+	Vec2d() : x(0.), y(0.) {}						// default constructor: default = (0.,0.)
+	Vec2d( double xx, double yy ) : x(xx), y(yy) {}
+
+//	~Vec2d();										// destructor			--implicit OK
+//	Vec2d operator=( Vec2d src& );					// assignment operator	--implicit OK
+//	Vec2d Vec2d( Vec2d src& );						// copy constructor		--implicit OK
+		// See p271 Stroustrup: "for types where the default copy constructor has the right
+		// semantics, I prefer to rely on that default."
+		// "I use a reference argument for the copy constructor because I must."
+
+	// Only put functions in the class that modify the data members (eg +=).
+	// Other functions that just produce new data values are defined outside the class.
+	// See p267 Stroustrup.  
+	Vec2d operator+=( Vec2d b )			{ x += b.x;     y += b.y;     return *this; }
+	Vec2d operator-=( Vec2d b )			{ x -= b.x;     y -= b.y;     return *this; }
+
+	Vec2d operator+=( double scalar )	{ x += scalar;  y += scalar;  return *this; }
+	Vec2d operator-=( double scalar )	{ x -= scalar;  y -= scalar;  return *this; }
+	Vec2d operator*=( double scalar )	{ x *= scalar;  y *= scalar;  return *this; }
+	Vec2d operator/=( double scalar )	{ x /= scalar;  y /= scalar;  return *this; }
+
+	Vec2d	rotate( double angle );
+};
+
+// non-member functions
+Vec2d	operator-( Vec2d a);
+
+Vec2d	operator+( Vec2d a, Vec2d b);
+Vec2d	operator+( Vec2d a, double s);
+Vec2d	operator+( double s, Vec2d b);
+
+Vec2d	operator-( Vec2d a, Vec2d b);
+Vec2d	operator-( Vec2d a, double s);
+Vec2d	operator-( double s, Vec2d b);
+
+Vec2d	operator*( Vec2d a, double s);
+Vec2d	operator/( Vec2d a, double s);
+
+bool	operator==( Vec2d a, Vec2d b);
+bool	operator!=( Vec2d a, Vec2d b);
+bool	operator<(  Vec2d a, Vec2d b);
+
+double	dotProduct( Vec2d a, Vec2d b);
+
+
+#endif VEC2D_INCLUDED
