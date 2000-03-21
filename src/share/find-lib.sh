325cdad4762516d6da8e250e981515625e07bda8
weigle <>
2000-03-21 19:29:12 +0000

    Aron pointed out a problem with which libraries are found first in some cases. We came to the following decision:
    If you have the source for libX checked out, it will use that copy,
    even if you haven't compiled your copy of the source. If you never
    compile your copy, linking will fail.
    
    If you want some other version of libX, say the one in stm/src, either
    delete your source or use MY_ALT_LIB_DIR.

--- src/share/find-lib.sh
+++ src/share/find-lib.sh
@@ -21,44 +21,6 @@ OBJ1=obj/$HW_OS/$LIB1
 SRC2=src/$LIB2
 OBJ2=obj/$HW_OS/$LIB2
 
-#echo 1>&2 "Looking for lib$TARGET.a ..."
-
-if test -e "$OBJECTS/$LIB1/lib$TARGET.a"; then
-   echo 1>&2 "Found lib$TARGET.a in $OBJECTS/$LIB1";
-   echo "-I$SOURCES/$SRC1 -L$OBJECTS/$LIB1"
-   exit 0;
-fi
-
-if test -e "$OBJECTS/$LIB2/lib$TARGET.a"; then
-   echo 1>&2 "Found lib$TARGET.a in $OBJECTS/$LIB2";
-   echo "-I$SOURCES/$SRC2 -L$OBJECTS/$LIB2"
-   exit 0;
-fi
-
-for path in $* ; do
-   if test -e "$path/$TARGET/$HW_OS/lib$TARGET.a" ; then
-      echo 1>&2 "Found lib$TARGET.a in $path/$TARGET/$HW_OS";
-      echo "-I$path/$TARGET -L$path/$TARGET/$HW_OS"
-      exit 0;
-   fi
-
-   if test -e "$path/$OBJ1/lib$TARGET.a" ; then
-      echo 1>&2 "Found lib$TARGET.a in $path/$OBJ1";
-      echo "-I$path/$SRC1 -L$path/$OBJ1"
-      exit 0;
-   fi
-
-   if test -e "$path/$OBJ2/lib$TARGET.a" ; then
-      echo 1>&2 "Found lib$TARGET.a in $path/$OBJ2";
-      echo "-I$path/$SRC2 -L$path/$OBJ2"
-      exit 0;
-   fi
-done
-
-#echo 1>&2 "Failed to locate lib$TARGET.a ... perhaps it has not been compiled?"
-
-#echo 1>&2 "Looking for $TARGET directory ..."
-
 if test -d "$SOURCES/$SRC1/"; then
    echo 1>&2 "Found $TARGET in $SOURCES/$SRC1";
    echo "-I$SOURCES/$SRC1 -L$OBJECTS/$LIB1"
@@ -73,20 +35,20 @@ fi
 
 for path in $* ; do
    if test -d "$path/$TARGET/" ; then
-      echo 1>&2 "Found $TARGET in $path";
-      echo "-I$path/$TARGET"
+      echo 1>&2 "Found $TARGET in $path/$TARGET";
+      echo "-I$path/$TARGET -L$path/$TARGET/$HW_OS"
       exit 0;
    fi
 
    if test -d "$path/$SRC1/" ; then
       echo 1>&2 "Found $TARGET in $path/$SRC1";
-      echo "-I$path/$SRC1"
+      echo "-I$path/$SRC1 -L$path/$LIB1"
       exit 0;
    fi
 
    if test -d "$path/$SRC2/" ; then
       echo 1>&2 "Found $TARGET in $path/$SRC2";
-      echo "-I$path/$SRC2"
+      echo "-I$path/$SRC2 -L$path/$LIB2"
       exit 0;
    fi
 done
