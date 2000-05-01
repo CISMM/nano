e5498e2c7fcfaec72171ef64752756101698a1fe
hudson <>
2000-05-01 15:27:05 +0000

    Now that nmm_Microscope compiles them, I figured that I ought to commit them. Adam's nmb_Device_Client class with a vrpn_Mutex soldered on so that only one client can access the device at a time.

---
+++ src/lib/nmBase/nmb_SharedDevice.C
@@ -0,0 +1,106 @@
+#include "nmb_SharedDevice.h"
+
+nmb_SharedDevice::nmb_SharedDevice (const char * name, int portForMutexServer,
+                      vrpn_Connection * connectionToDeviceServer,
+                      const char * NICaddress) :
+    nmb_Device_Client (name, connectionToDeviceServer),
+    d_mutex (name, portForMutexServer, NICaddress) {
+
+  d_mutex.addRequestGrantedCallback(this, handle_gotMutex);
+
+fprintf(stderr, "nmb_SharedDevice:  requesting the lock on %s.\n", d_myName);
+
+  // We're currently running standalone;  grab the mutex.
+  d_mutex.request();
+
+}
+
+// virtual
+nmb_SharedDevice::~nmb_SharedDevice (void) {
+
+
+}
+
+
+
+int nmb_SharedDevice::mainloop (void) {
+  d_mutex.mainloop();
+  return 0;
+}
+
+void nmb_SharedDevice::requestMutex (void) {
+
+fprintf(stderr, "nmb_SharedDevice:  requesting the lock on %s.\n", d_myName);
+
+  d_mutex.request();
+
+}
+
+void nmb_SharedDevice::releaseMutex (void) {
+
+fprintf(stderr, "nmb_SharedDevice:  releasing the lock on %s.\n", d_myName);
+
+  d_mutex.release();
+
+}
+
+void nmb_SharedDevice::addPeer (const char * stationName) {
+
+fprintf(stderr, "nmb_SharedDevice:  releasing the lock on %s "
+"to add a peer named %s.\n", d_myName, stationName);
+
+  d_mutex.release();
+  d_mutex.addPeer(stationName);
+
+}
+
+
+// virtual
+long nmb_SharedDevice::dispatchMessage (long len, const char * buf,
+                                        vrpn_int32 type) {
+
+  if (d_mutex.isHeldLocally()) {
+    return nmb_Device_Client::dispatchMessage(len, buf, type);
+  }
+
+fprintf(stderr, "nmb_SharedDevice:  throwing out a message\n"
+"  (type %d, to %s) since we don't have the lock.\n", type, d_myName);
+
+  return 0;
+}
+
+
+void nmb_SharedDevice::registerGotMutexCallback
+            (void * userdata,
+             void (* f) (void *, nmb_SharedDevice *)) {
+  sharedDeviceCallback * cb;
+
+  cb = new sharedDeviceCallback;
+  if (!cb) {
+    fprintf(stderr, "nmb_SharedDevice::registerGotMutexCallback:  "
+                    "Out of memory.\n");
+    return;
+  }
+
+  cb->f = f;
+  cb->userdata = userdata;
+  cb->next = d_gotMutexCallbacks;
+  d_gotMutexCallbacks = cb;
+
+}
+
+// static
+int nmb_SharedDevice::handle_gotMutex (void * userdata) {
+  nmb_SharedDevice * me = (nmb_SharedDevice *) userdata;
+  sharedDeviceCallback * cb;
+
+fprintf(stderr, "nmb_SharedDevice:  got the lock on %s.\n", me->d_myName);
+
+  for (cb = me->d_gotMutexCallbacks;  cb;  cb = cb->next) {
+    (cb->f)(cb->userdata, me);
+  }
+
+  return 0;
+}
+
+
---
+++ src/lib/nmBase/nmb_SharedDevice.h
@@ -0,0 +1,69 @@
+#ifndef NMB_SHARED_DEVICE_H
+#define NMB_SHARED_DEVICE_H
+
+#include "nmb_Device.h"
+
+#include <vrpn_Mutex.h>
+//class vrpn_Mutex;  // from <vrpn_Mutex.h>
+
+/// nmb_SharedDevice
+///
+///   A subclass of nmb_Device_Client that uses a vrpn_Mutex to ensure that
+/// only one instance of the class is sending commands to the Device_Server
+/// at any one time.  Calls to dispatchMessage() at an instance which
+/// doesn't have the mutex are ignored.
+
+// This version assumes vrpn_Mutex 1.3 and has a couple of workarounds in
+// place:
+//   We start up with a mutex and request() it immediately.  Since there
+// are no peers, we have it, so it doesn't interfere with standalone work.
+//   Before we call vrpn_Mutex::addPeer(), we release() the mutex.
+
+// At some future time we'll want to extend (or further subclass) to
+// make the mutex only restrict a subset of message types sent.
+
+class nmb_SharedDevice : public nmb_Device_Client {
+
+  public:
+
+    nmb_SharedDevice (const char * name, int portForMutexServer,
+                      vrpn_Connection * connectionToDeviceServer = NULL,
+                      const char * mutexNICaddress = NULL);
+    virtual ~nmb_SharedDevice (void);
+
+    virtual int mainloop (void);
+      ///< MUST be called by subclasses!
+
+    void requestMutex (void);
+    void releaseMutex (void);
+    void addPeer (const char * stationName);
+      ///< Takes a vrpn station name, of the form "<host>:<port>".
+
+    virtual long dispatchMessage (long len, const char * buf, vrpn_int32 type);
+      ///< If we don't have the mutex, returns 0;  otherwise, passes the
+      ///< message on to nmb_Device_Client::dispatchMessage() to be buffered
+      ///< or sent as appropriate.
+
+    void registerGotMutexCallback (void * userdata,
+                                   void (*) (void *, nmb_SharedDevice *));
+      ///< Lets the using process take some action to notify the user
+      ///< when the lock is secured.
+
+  private:
+
+    vrpn_Mutex d_mutex;
+
+    struct sharedDeviceCallback {
+      void (* f) (void *, nmb_SharedDevice *);
+      void * userdata;
+      sharedDeviceCallback * next;
+    };
+
+    sharedDeviceCallback * d_gotMutexCallbacks;
+
+    static int handle_gotMutex (void *);
+
+};
+
+#endif  // NMB_SHARED_DEVICE_H
+
