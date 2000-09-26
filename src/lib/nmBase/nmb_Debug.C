88b1f53be757292eaf4f8018b72b5527494bd72f
hudson <>
2000-09-26 19:39:57 +0000

    OK, so after this morning I wrote a function using varargs to give me debugging output for collaboration without having to comment & uncomment fprintfs all the time.  It's not a perfect solution, but better than nothing, and probably as good as is practical.
    	* nmb_Debug.C : add collab_verbosity, collabVerbose()

--- src/lib/nmBase/nmb_Debug.C
+++ src/lib/nmBase/nmb_Debug.C
@@ -1,3 +1,18 @@
+#include <stdio.h>  // for vfprintf()
+#include <stdarg.h>  // for va_start(), va_end(), va_list
 
 int spm_verbosity;
+int collab_verbosity;
+
+void collabVerbose (int level, char * msg, ...) {
+  va_list argptr;
+
+  if (collab_verbosity >= level) {
+    va_start(argptr, msg);
+    vfprintf(stderr, msg, argptr)
+    va_end(argptr);
+  }
+
+}
+
 
--- src/lib/nmBase/nmb_Debug.h
+++ src/lib/nmBase/nmb_Debug.h
@@ -2,9 +2,12 @@
 #define NMB_DEBUG_H
 
 extern int spm_verbosity;
+extern int collab_verbosity;
 
 #define VERBOSE(level,msg) \
     if (spm_verbosity >= level) fprintf(stderr,"%s\n",msg)
+
+void collabVerbose (int level, char * msg, ...);
 
 
 #endif  // NMB_DEBUG_H
