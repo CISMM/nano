e25179743f4347ab9d229abf778f6a04a7c8468a
marshbur <>
2001-06-04 19:46:53 +0000

    CollaborationManager.C:  changes to accomodate new PlaneSync.
    microscape.c:  changes to use new calculated plane types.
    
    nmui_PlaneSync.C, .h:
    	- removed AFMState data member, as plane names are now
    	  kept in nmb_Dataset.
    	- moved into the various calculated plane classes the code for
    	  plane creation as a result of an incoming vrpn message
    	- likewise for sending vrpn messages about new planes
    	  defined locally.
    	- PlaneSync now just keeps track of whether or not nano is
    	  currently accepting and sending calculated planes, depending
    	  on whether nano is synched with a peer.

--- src/app/nano/lib/nmUI/nmui_PlaneSync.C
+++ src/app/nano/lib/nmUI/nmui_PlaneSync.C
@@ -1,244 +1,206 @@
 #include "nmui_PlaneSync.h"
 
+// static class members
+const char nmui_PlaneSync::
+vrpnSenderType[] = "Plane Synch";
 
-nmui_PlaneSync::nmui_PlaneSync (nmb_Dataset * d, AFMDataset * /*afmd*/, 
-				vrpn_Connection * c) :
-    d_server (c),
+const char nmui_PlaneSync::
+vrpnMessageType[] = "Plane Synch Calculated Plane";
+
+
+nmui_PlaneSync::nmui_PlaneSync( nmb_Dataset * dataset, 
+				 vrpn_Connection * conn ) :
+    d_server (conn),
     d_peer (NULL),
-    d_dataset (d),
-    //    d_afmdata (afmd),
-    d_sync_hostname (NULL),
+    d_dataset (dataset),
     d_accepting (VRPN_FALSE),
-    d_outgoing_flat_list (NULL),
-    d_incoming_flat_list (NULL)
-
+    incomingCalcdPlaneList (NULL),
+    outgoingCalcdPlaneList (NULL)
 {
-
-  d_myId = c->register_sender("nmui Plane Sync");
-
-  d_flatPlaneSync_type = c->register_message_type("nmui PS Flat Plane");
-
-  c->register_handler(d_flatPlaneSync_type, handle_flatPlaneSync,
-                      this, d_myId);
-
-  d->registerFlatPlaneCallback((void *)this, queueFlatPlaneForSync);
-
-}
-
-nmui_PlaneSync::~nmui_PlaneSync (void) {
-  if (d_sync_hostname)
-      delete [] d_sync_hostname;
-}
+  d_senderID = conn->register_sender( vrpnSenderType );
+  d_synchCalculatedPlaneMessageType 
+    = conn->register_message_type( vrpnMessageType );
+  conn->register_handler(d_synchCalculatedPlaneMessageType, 
+		      handleCalculatedPlaneSync,
+                      this, d_senderID);
+  nmb_CalculatedPlane::registerNewCalculatedPlaneCallback( (void *) this, 
+						queueCalculatedPlaneForSync );
+} // end nmui_PlaneSync( ... )
 
 
-void nmui_PlaneSync::addPeer (vrpn_Connection * c,
-			      const char * new_sync_hostname) {
 
+nmui_PlaneSync::~nmui_PlaneSync( void ) 
+{
+  // remove any items from our lists of calculated planes
+  if( incomingCalcdPlaneList != NULL )
+    {
+      vrpn_HANDLERPARAM_node* pnode = incomingCalcdPlaneList;
+      while( pnode != NULL )
+	{
+	  delete (void*) pnode->data->buffer;
+	  delete pnode->data;
+	  incomingCalcdPlaneList = pnode->next;
+	  delete pnode;
+	  
+	  pnode = incomingCalcdPlaneList;
+	}
+    } 
+
+  if( outgoingCalcdPlaneList != NULL )
+    {
+      nmb_CalculatedPlaneNode* cnode = outgoingCalcdPlaneList;
+      while( cnode != NULL )
+	{
+	  outgoingCalcdPlaneList = cnode->next;
+	  delete cnode;
+	  cnode = outgoingCalcdPlaneList;
+	}
+    }
+
+  // unregister our callback
+  nmb_CalculatedPlane::removeNewCalculatedPlaneCallback( (void*) this,
+					     queueCalculatedPlaneForSync );
+} // end ~nmui_PlaneSynch
+ 
+
+
+
+void nmui_PlaneSync::changePeer( vrpn_Connection * c ) 
+{
   d_peer = c;
-  if (new_sync_hostname) {
-      if (d_sync_hostname) {
-	  delete [] d_sync_hostname;
-      }
-      d_sync_hostname = new char [strlen(new_sync_hostname) +1];
-      strcpy(d_sync_hostname, new_sync_hostname);
-  }
-//fprintf(stderr, "nmui_PlaneSync::addPeer().\n");
-  d_myId = d_peer->register_sender("nmui Plane Sync");
-  d_flatPlaneSync_type = d_peer->register_message_type("nmui PS Flat Plane");
-
-}
-
-
-// static
-int nmui_PlaneSync::handle_flatPlaneSync (void * userdata,
-                                          vrpn_HANDLERPARAM p) {
-  nmui_PlaneSync * it = (nmui_PlaneSync *) userdata;
-  char outputPlane [256];
-  char inputPlane [256];
-  const char * bp = p.buffer;
-  vrpn_int32 outputPlaneLen;
-  vrpn_int32 inputPlaneLen;
-  vrpn_float64 dx;
-  vrpn_float64 dy;
-  vrpn_float64 offset;
-
-  vrpn_unbuffer(&bp, &dx);
-  vrpn_unbuffer(&bp, &dy);
-  vrpn_unbuffer(&bp, &offset);
-  vrpn_unbuffer(&bp, &outputPlaneLen);
-  vrpn_unbuffer(&bp, &inputPlaneLen);
-  vrpn_unbuffer(&bp, outputPlane, outputPlaneLen);
-  vrpn_unbuffer(&bp, inputPlane, inputPlaneLen);
-  outputPlane[outputPlaneLen] = 0;
-  inputPlane[inputPlaneLen] = 0;
-
-//fprintf(stderr, "In nmui_PlaneSync::handle_flatPlaneSync. out %s in %s\n",
-//	outputPlane, inputPlane);
-
-//   char new_outputPlane[256];
-//   if (it->d_sync_hostname) {
-//       sprintf(new_outputPlane, "%s from %s", outputPlane, it->d_sync_hostname);
-//   } else {
-//       sprintf(new_outputPlane, "%s from ??", outputPlane);
-//   }
-  if (it->d_accepting) {
-      // Add the host name to the plane name so we can distinguish
-      // where the plane came from
-      it->d_dataset->computeFlattenedPlane(outputPlane, inputPlane,
-					   dx, dy, offset);
-      it->d_dataset->inputPlaneNames->addEntry(outputPlane);
-  } else {
-      // Create the flatten plane, but don't tell the user about it. 
-      // This is cribbed from nmb_Dataset.C::computeFlattenPlane
-      BCPlane * plane = it->d_dataset->inputGrid->getPlaneByName(inputPlane);
-      if (plane == NULL) {
-	  fprintf(stderr,
-		  "nmui_PlaneSync::handle_flatPlaneSync(): could not get height plane!\n");
-	  return -1;
-      }
-      char        newunits [1000];
-      sprintf(newunits, "%s_flat", plane->units()->Characters());
-      BCPlane * outplane = it->d_dataset->inputGrid->addNewPlane(outputPlane, newunits, NOT_TIMED);
-      if (outplane == NULL) {
-          fprintf(stderr,
-            "nmui_PlaneSync::handle_flatPlaneSync(): Can't make plane %s\n",outputPlane);
-          return -1;
-      }
-      it->d_dataset->dataImages->addImage(new nmb_ImageGrid(outplane));
-
-      //Add flat plane to the list. It will be computed if/when the user
-      // decides to sync with their collaborator. 
-      flatten_node * new_node = new flatten_node;
-      new_node->next = NULL;
-      new_node->data = new flatten_data;
-      new_node->data->dx = dx;
-      new_node->data->dy = dy;
-      new_node->data->offset = offset;
-      new_node->data->from_plane = plane;
-      new_node->data->flat_plane = outplane;
-
-      // add to the end of the list, because plane creation order
-      // may be important!!!
-      if (it->d_incoming_flat_list==NULL) {
-	  it->d_incoming_flat_list = new_node;
-      } else {
-	  flatten_node * list_itr =it->d_incoming_flat_list; 
-	  while (list_itr->next !=NULL) {
-	      list_itr = list_itr->next;
-	  }
-	  list_itr->next = new_node;
-      }
-  }
-  return 0;
-}
-
-
-// static
-void nmui_PlaneSync::queueFlatPlaneForSync (void * userdata, const flatten_data * f) {
-
-  nmui_PlaneSync * it = (nmui_PlaneSync *) userdata;
-
-  // HACK
-  // If we don't have a peer, or just generally, we should queue up the
-  // plane creation request until the user chooses to synchronize.
-
-  if ((it->d_peer == NULL)||(!it->d_accepting) ) {
-//fprintf(stderr, "queueFlatPlaneForSync() with no peer.\n");
-      //Add flat plane to the list. It will be communicated if/when the user
-      // decides to sync with their collaborator. 
-      flatten_node * new_node = new flatten_node;
-      new_node->next = NULL;
-      new_node->data = new flatten_data;
-      new_node->data->dx = f->dx;
-      new_node->data->dy = f->dy;
-      new_node->data->offset = f->offset;
-      new_node->data->from_plane = f->from_plane;
-      new_node->data->flat_plane = f->flat_plane;
-
-      // add to the end of the list, because plane creation order
-      // may be important!!! 
-      if (it->d_outgoing_flat_list==NULL) {
-	  it->d_outgoing_flat_list = new_node;
-      } else {
-	  flatten_node *list_itr =it->d_outgoing_flat_list; 
-	  while (list_itr->next !=NULL) {
-	      list_itr = list_itr->next;
-	  }
-	  list_itr->next = new_node;
-      }
-  } else {
-      // Send the plane information right now.
-      it->sendFlatMessage(f);
-//fprintf(stderr, "nmui_PlaneSync::queueFlatPlaneForSync sent.\n");
-  }
-}
-
-int nmui_PlaneSync::sendFlatMessage(const flatten_data * f) 
-{
-  char planemsg [1024];
-  timeval now;
-  char * bufptr = planemsg;
-  vrpn_int32 msglen = 1024;
-
-  vrpn_buffer(&bufptr, &msglen, f->dx);
-  vrpn_buffer(&bufptr, &msglen, f->dy);
-  vrpn_buffer(&bufptr, &msglen, f->offset);
-  vrpn_buffer(&bufptr, &msglen,
-                  (vrpn_int32) f->flat_plane->name()->Length());
-  vrpn_buffer(&bufptr, &msglen,
-                  (vrpn_int32) f->from_plane->name()->Length());
-  vrpn_buffer(&bufptr, &msglen, f->flat_plane->name()->Characters(),
-	      f->flat_plane->name()->Length());
-  vrpn_buffer(&bufptr, &msglen, f->from_plane->name()->Characters(),
-	      f->from_plane->name()->Length());
+  d_senderID = d_peer->register_sender( vrpnSenderType );
+  d_synchCalculatedPlaneMessageType 
+    = d_peer->register_message_type( vrpnMessageType );
   
-  gettimeofday(&now, NULL);
-  d_peer->pack_message(1024 - msglen, now, d_flatPlaneSync_type, d_myId,
-		       planemsg, vrpn_CONNECTION_RELIABLE);
+} // end addPeer( ... )
+
+
+
+// static
+int nmui_PlaneSync::
+handleCalculatedPlaneSync( void * userdata, vrpn_HANDLERPARAM p ) 
+{
+  nmui_PlaneSync * it = (nmui_PlaneSync *) userdata;
+
+  if (it->d_accepting) 
+    {
+      nmb_CalculatedPlane* newPlane = NULL;
+      try
+	{
+	  newPlane = 
+	    nmb_CalculatedPlane::receiveCalculatedPlane( p, it->d_dataset );
+	}
+      catch( nmb_CalculatedPlaneCreationException e )
+	{
+
+	}
+
+      if( newPlane == NULL )
+	{
+	  fprintf( stderr, "nmui_PlaneSync::Unable to create "
+		   "plane from remote.\n" );
+	}
+      
+      const char* newPlaneName
+	= newPlane->getName( )->Characters( );
+      it->d_dataset->inputPlaneNames->addEntry( newPlaneName );
+    } 
+  else // not accepting updates
+    {
+      // copy vrpn_HANDLERPARAM p and use it when we are accepting
+      vrpn_HANDLERPARAM* newParam = new vrpn_HANDLERPARAM;
+      newParam->type = p.type;
+      newParam->sender = p.sender;
+      newParam->msg_time = p.msg_time;
+      newParam->payload_len = p.payload_len;
+      char* newBuffer = new char[ p.payload_len * sizeof( char ) ];
+      memcpy( newBuffer, p.buffer, p.payload_len );
+      newParam->buffer = newBuffer;
+
+      // put it at the end of the list, since plane creation
+      // order may be important!
+      vrpn_HANDLERPARAM_node* newNode = new vrpn_HANDLERPARAM_node;
+      newNode->data = newParam;
+      newNode->next = NULL;
+      if( it->incomingCalcdPlaneList == NULL )
+	it->incomingCalcdPlaneList = newNode;
+      else
+	{
+	  for( vrpn_HANDLERPARAM_node* last = it->incomingCalcdPlaneList;
+	       last->next != NULL; last = last->next );
+	  last->next = newNode;
+	}
+    }
   return 0;
-}
+} // end handleCalculatedPlaneSynch
+
+
+
+// static
+void nmui_PlaneSync::
+queueCalculatedPlaneForSync( void* userdata, const nmb_CalculatedPlane* newPlane )
+{
+  nmui_PlaneSync * it = (nmui_PlaneSync *) userdata;
+
+  if( it->d_peer != NULL && it->d_accepting ) // just send the plane now
+    {
+      newPlane->sendCalculatedPlane( it->d_peer, it->d_senderID, 
+				     it->d_synchCalculatedPlaneMessageType );
+    }
+  else  // queue the plane for synch later
+    {
+      nmb_CalculatedPlaneNode* node = new nmb_CalculatedPlaneNode;
+      node->data = (nmb_CalculatedPlane*) newPlane;
+      node->next = NULL;
+      
+      // add plane to the end of the list, as plane-creation
+      // order is likely important
+      if( it->outgoingCalcdPlaneList == NULL )
+	it->outgoingCalcdPlaneList = node;
+      else
+	{
+	  for( nmb_CalculatedPlaneNode* last = it->outgoingCalcdPlaneList;
+	       last->next != NULL; last = last->next );
+	  last->next = node;
+	}
+    }
+} // end queueCalculatedPlaneForSync( ... )
+
+
 
 void nmui_PlaneSync::acceptUpdates (void) 
 {
-    // If there is no peer, this doesn't make sense!
     if (!d_peer) return;
-
     d_accepting = VRPN_TRUE;
+
     // send any planes that we have created to our peer
-    flatten_node *curr_node = d_outgoing_flat_list;
-    flatten_node *last_node;
-    while (curr_node != NULL) {
-	sendFlatMessage(curr_node->data);
-	last_node = curr_node;
-	curr_node = curr_node->next;
-	delete last_node->data;
-	delete last_node;
-    }
-    d_outgoing_flat_list = NULL;
-    // calculate any planes that our peer has send us. 
-    curr_node = d_incoming_flat_list;
-    while (curr_node != NULL) {
-	// We already added a plane with the right name when
-	// we received the message, so computing the plane now
-	// will fill in that plane, and register callbacks so 
-	// it gets updated.
-	d_dataset->computeFlattenedPlane(
-		 curr_node->data->flat_plane->name()->Characters(), 
-		 curr_node->data->from_plane->name()->Characters(),
-		 curr_node->data->dx, 
-		 curr_node->data->dy, 
-		 curr_node->data->offset);
-	// Now we want the user to see the plane.
-	d_dataset->inputPlaneNames->addEntry(
-		 curr_node->data->flat_plane->name()->Characters());
-	last_node = curr_node;
-	curr_node = curr_node->next;
-	delete last_node->data;
-	delete last_node;
-    }
-    d_incoming_flat_list = NULL;
-    return;
-}
+    nmb_CalculatedPlaneNode* cnode = outgoingCalcdPlaneList;
+    while( cnode != NULL )
+      {
+	queueCalculatedPlaneForSync( this, cnode->data );
+
+	// remove the node now.
+	outgoingCalcdPlaneList = cnode->next;
+	delete cnode;
+	cnode = outgoingCalcdPlaneList;
+      }
+
+    // create any planes that our peer sent us. 
+    vrpn_HANDLERPARAM_node* pnode = incomingCalcdPlaneList;
+    while( pnode != NULL )
+      {
+	handleCalculatedPlaneSync( this, *(pnode->data) );
+	
+	// remove the node now.
+	delete (void*) pnode->data->buffer;
+	delete pnode->data;
+	incomingCalcdPlaneList = pnode->next;
+	delete pnode;
+	
+	pnode = incomingCalcdPlaneList;
+      }
+} // end acceptUpdates()
+
 
 void nmui_PlaneSync::queueUpdates (void) 
 {
--- src/app/nano/lib/nmUI/nmui_PlaneSync.h
+++ src/app/nano/lib/nmUI/nmui_PlaneSync.h
@@ -3,60 +3,59 @@
 
 #include <vrpn_Connection.h>  // for vrpn_HANDLERPARAM
 #include <nmb_Dataset.h>  // for flatten_data, etc.
-#include <AFMState.h>  // for list of input planes.
+#include <nmb_CalculatedPlane.h>
 
 // NANOX FLAT
 
 /// Class handles synchronization of derived planes between nM replicas.
 class nmui_PlaneSync {
+public:
+  
+  nmui_PlaneSync( nmb_Dataset *, vrpn_Connection * );
+  ~nmui_PlaneSync( void );
+  
+  /// Start collaborating with someone, and sharing plane creations.
+  void changePeer( vrpn_Connection * );
+  
+  /// When the user is synced to a collaborator, call these.
+  /// acceptUpdates will send any flat planes we have created to our peer,
+  /// and create any flat planes our peer has sent us.
+  void acceptUpdates( void );
+  
+  /// queueUpdates will set a flag so plane creations are queued again.
+  void queueUpdates( void );
+  
+protected:
+  
+  static const char vrpnSenderType[];
+  static const char vrpnMessageType[];
+  
+  vrpn_Connection * d_server;
+  vrpn_Connection * d_peer;
+  
+  vrpn_int32 d_senderID;
+  vrpn_int32 d_synchCalculatedPlaneMessageType;
+  
+  nmb_Dataset * d_dataset;
+  
+  /// Are we accepting new updates right now?
+  vrpn_bool d_accepting;
+  
+  static int handleCalculatedPlaneSync( void *, vrpn_HANDLERPARAM );
+  
+  static NewCalculatedPlaneCallback queueCalculatedPlaneForSync;
+  // see nmb_CalculatedPlane for definiton of this (function) type.
+  
+  // struct and data member for keeping incoming plane messages
+  // when we're not accepting
+  struct vrpn_HANDLERPARAM_node
+  {
+    vrpn_HANDLERPARAM* data;
+    vrpn_HANDLERPARAM_node* next;
+  };
+  vrpn_HANDLERPARAM_node* incomingCalcdPlaneList;
 
-  public:
-
-    nmui_PlaneSync (nmb_Dataset *, AFMDataset *, vrpn_Connection *);
-    ~nmui_PlaneSync (void);
-
-    /// Start collaborating with someone, and sharing plane creations.
-    void addPeer (vrpn_Connection *, const char * new_sync_hostname = NULL);
-
-    /// When the user is synced to a collaborator, call these.
-    /// acceptUpdates will send any flat planes we have created to our peer,
-    /// and create any flat planes our peer has sent us.
-    void acceptUpdates (void);
-    /// queueUpdates will set a flag so plane creations are queued again.
-    void queueUpdates (void);
-  protected:
-
-
-  private:
-
-    vrpn_Connection * d_server;
-    vrpn_Connection * d_peer;
-
-    vrpn_int32 d_myId;
-    vrpn_int32 d_flatPlaneSync_type;
-
-    nmb_Dataset * d_dataset;
-
-    AFMDataset * d_afmdata;
-
-    /// if we get planes from a collaborator, we identify them
-    /// by appending this hostname.
-    char * d_sync_hostname;
-
-    /// Are we accepting new updates right now?
-    vrpn_bool d_accepting;
-    
-    /// If we aren't accepting, we need to queue up flat plane 
-    /// creations, both ours to tell the collaborator, and incoming
-    /// flat planes from our collaborator.
-    flatten_node * d_outgoing_flat_list;
-    flatten_node * d_incoming_flat_list;
-
-    static int handle_flatPlaneSync (void *, vrpn_HANDLERPARAM);
-
-    static void queueFlatPlaneForSync (void *, const flatten_data *);
-    
-    int sendFlatMessage(const flatten_data *);
+  nmb_CalculatedPlaneNode* outgoingCalcdPlaneList;
 };
 
 #endif  // NMUI_PLANE_SYNC_H
