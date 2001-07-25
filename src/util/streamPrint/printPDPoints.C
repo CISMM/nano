6af1b1a9c74de6e1162e64620b6170abcbef55d0
hudson <>
2001-07-25 14:03:22 +0000

    More new analysis tools.

--- src/util/streamPrint/printPDPoints.C
+++ src/util/streamPrint/printPDPoints.C
@@ -1,17 +1,8 @@
-/// Print out data from a vrpn_ForceDevice_Remote outgoing streamfile.
-/// Lines are of the form "<timestamp> <a> <b> <c> <d>", where <a>-<d>
-/// are the parameters of the plane specified at <timestamp>.
+/// Print out data from a nmm_Microscope streamfile.
+/// Lines are of the form "<timestamp> <x> <y> <z>", where <x>-<z>
+/// are the positions of the point data reported at <timestamp>.
 /// Timestamps are all normalized so that streamfiles start at 0.
 /// Data to stdout, diagnostics to stderr.
-/// We could inherit from vrpn_ForceDevice to get access to encode,
-/// decode, and message type IDs, but instead reimplement those tiny
-/// pieces from scratch.  Memo to self:  inheritance != composition.
-
-#include <vrpn_Shared.h>
-#include <vrpn_Connection.h>
-#include <vrpn_FileConnection.h>
- 
-//#include <vrpn_ForceDevice.h>  // Not strictly necessary!
 
 #ifdef sgi
 #include <unistd.h>
@@ -22,16 +13,27 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <vrpn_Shared.h>
+#include <vrpn_Connection.h>
+#include <vrpn_FileConnection.h>
+ 
+// The "reuseful" thing to do would be to have a nmm_Microscope_Remote
+// and just use its point data callback facility, but nMR is VERY heavyweight
+// and drags a lot of other stuff along with it
+//#include <nmm_MicroscopeRemote.h>
 
 static void parseArguments (int argc, char ** argv);
 static void handle_cntl_c (int);
 
 
-static 	vrpn_Connection * connection = NULL;
-static   vrpn_File_Connection * fcon = NULL;
+static vrpn_Connection * connection = NULL;
+static vrpn_File_Connection * fcon = NULL;
 
 static int isReadingStreamFile = 0;
 static char * device_name = NULL;
+
+static vrpn_bool g_offsetInitialized = 0;
+static timeval g_offset;
 
 //--------------------------------------------------------------------------
 // Handles VRPN messages
@@ -40,37 +42,38 @@ static char * device_name = NULL;
 // we have to reproduce the code from vrpn_ForceDevice::decode_plane
 // here (or create a class that inherits from it).
 
-int handle_plane (void * userdata, vrpn_HANDLERPARAM p) {
-  const char * mptr;
-  vrpn_float32 a, b, c, d;
-  //vrpn_float32 kspring, kdamp, fdyn, fstat;
-  //vrpn_int32 index, cycles;
-  int i;
+int handle_point (void *, vrpn_HANDLERPARAM p) {
+  const char ** buf;
+  vrpn_float32 x, y, z;
+  vrpn_int32 sec, usec, fields;
   timeval elapsed;
 
-  mptr = p.buffer;
-  vrpn_unbuffer(&mptr, &a);
-  vrpn_unbuffer(&mptr, &b);
-  vrpn_unbuffer(&mptr, &c);
-  vrpn_unbuffer(&mptr, &d);
-  //vrpn_unbuffer(&mptr, &kspring);
-  //vrpn_unbuffer(&mptr, &kdamp);
-  //vrpn_unbuffer(&mptr, &fdyn);
-  //vrpn_unbuffer(&mptr, &fstat);
-  //vrpn_unbuffer(&mptr, &index);
-  //vrpn_unbuffer(&mptr, &cycles);
+  // Elapsed time from streamfiles is actually elapsed time since
+  // the first user data.  So we hack here uglyly to display the
+  // elapsed time since the first data we output.
 
   connection->time_since_connection_open(&elapsed);
 
-  printf("%d.%06d %.6f %.6f %.6f %.6f\n",
-         elapsed.tv_sec, elapsed.tv_usec, a, b, c, d);
-  //printf("%d.%06d %.6f %.6f %.6f %.6f\n",
-         //p.msg_time.tv_sec, p.msg_time.tv_usec, a, b, c, d);
+  if (!g_offsetInitialized) {
+    g_offsetInitialized = 1;
+    g_offset = elapsed;
+  }
+
+  elapsed = vrpn_TimevalDiff(elapsed, g_offset);
+
+  buf = &p.buffer;
+  vrpn_unbuffer(buf, &x);
+  vrpn_unbuffer(buf, &y);
+  vrpn_unbuffer(buf, &sec);
+  vrpn_unbuffer(buf, &usec);
+  vrpn_unbuffer(buf, &fields);
+  vrpn_unbuffer(buf, &z);
+
+  printf("%d.%06d %.6f %.6f %.6f\n",
+         elapsed.tv_sec, elapsed.tv_usec, x, y, z);
 
   return 0;
 }
-
-
 
 
 // Argument handling
@@ -99,9 +102,11 @@ void parseArguments(int argc, char ** argv) {
 }
 
 int main (unsigned argc, char ** argv) {
-  vrpn_int32 plane_id;
-  vrpn_bool done = VRPN_FALSE;
+
+  vrpn_int32 resultData_type;
+  vrpn_bool done;
     
+  done = VRPN_FALSE;
   parseArguments(argc, argv);
     
   // Initialize our connections to the things we are going to control.
@@ -125,8 +130,9 @@ int main (unsigned argc, char ** argv) {
 
   signal(SIGINT, handle_cntl_c);
 
-  plane_id = connection->register_message_type("vrpn_ForceDevice Plane");
-  connection->register_handler(plane_id, handle_plane, NULL);
+  resultData_type = connection->register_message_type
+                           ("nmm Microscope PointResultData");
+  connection->register_handler(resultData_type, handle_point, NULL);
 
   while (!done) {
     //sleep(0.01);
