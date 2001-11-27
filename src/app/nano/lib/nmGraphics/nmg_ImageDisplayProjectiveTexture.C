5f411fe20ab01f60203d6b494b5ec5b83709e2fc
seeger <>
2001-11-27 16:42:56 +0000

    2001-11-27  Adam Seeger  <seeger@cs.unc.edu>
            * nmg_ImageDisplayProjectiveTexture.Ch: new files to provide a more
            abstract interface to the alignment texture

---
+++ src/app/nano/lib/nmGraphics/nmg_ImageDisplayProjectiveTexture.C
@@ -0,0 +1,64 @@
+#include "nmg_ImageDisplayProjectiveTexture.h"
+
+nmg_ImageDisplayProjectiveTexture::nmg_ImageDisplayProjectiveTexture
+   (nmg_Graphics *g):
+  nmb_ImageDisplay(),
+  d_graphicsDisplay(g), d_projectiveTexturesEnabled(vrpn_FALSE)
+{
+
+}
+
+void nmg_ImageDisplayProjectiveTexture::addImageToDisplay(nmb_Image *image)
+{
+  if (!d_graphicsDisplay) return;
+  d_graphicsDisplay->setTextureMode(nmg_Graphics::COLORMAP,
+                                    nmg_Graphics::REGISTRATION_COORD);
+  d_projectiveTexturesEnabled = vrpn_TRUE;
+}
+
+void nmg_ImageDisplayProjectiveTexture::removeImageFromDisplay(nmb_Image *
+                                                            /*image*/)
+{
+  if (!d_graphicsDisplay) return;
+  if (!d_projectiveTexturesEnabled) return;
+  
+  // this check is very important because we don't want to disable textures
+  // for some other mode
+  if (d_graphicsDisplay->getTextureMode() != nmg_Graphics::COLORMAP) return;
+
+  d_graphicsDisplay->setTextureMode(nmg_Graphics::NO_TEXTURES,
+                                    nmg_Graphics::RULERGRID_COORD);
+  d_projectiveTexturesEnabled = vrpn_FALSE;
+}
+
+void nmg_ImageDisplayProjectiveTexture::updateDisplayTransform(
+                                       nmb_Image * /*image*/,
+                                       double *transform)
+{
+  if (!d_graphicsDisplay) return;
+
+  d_graphicsDisplay->setTextureTransform(transform);
+}
+
+void nmg_ImageDisplayProjectiveTexture::setDisplayColorMap(
+                         nmb_Image * /*image*/,
+                         const char *map, const char *mapdir)
+{
+  if (!d_graphicsDisplay) return;
+  d_graphicsDisplay->setRealignTexturesConversionMap(map, mapdir);
+}
+
+void nmg_ImageDisplayProjectiveTexture::setDisplayColorMapRange(
+                        nmb_Image * /*image*/,
+                        float data_min, float data_max,
+                        float color_min, float color_max)
+{
+  if (!d_graphicsDisplay) return;
+  d_graphicsDisplay->setRealignTextureSliderRange(data_min, data_max,
+                                                    color_min, color_max);
+}
+
+void nmg_ImageDisplayProjectiveTexture::updateImage(nmb_Image *image) 
+{
+  d_graphicsDisplay->createRealignTextures(image->name()->Characters());
+}
---
+++ src/app/nano/lib/nmGraphics/nmg_ImageDisplayProjectiveTexture.h
@@ -0,0 +1,25 @@
+#ifndef NMG_IMAGEDISPLAYPROJECTIVETEXTURE_H
+#define NMG_IMAGEDISPLAYPROJECTIVETEXTURE_H
+
+#include "nmb_ImageDisplay.h"
+#include "nmg_Graphics.h"
+
+class nmg_ImageDisplayProjectiveTexture: public nmb_ImageDisplay {
+ public:
+  nmg_ImageDisplayProjectiveTexture(nmg_Graphics *g);
+  virtual void addImageToDisplay(nmb_Image *image);
+  virtual void removeImageFromDisplay(nmb_Image *image);
+  virtual void updateDisplayTransform(nmb_Image *image, double *transform);
+  virtual void setDisplayColorMap(nmb_Image *image,
+                       const char *map, const char *mapdir);
+  virtual void setDisplayColorMapRange(nmb_Image *image,
+                       float data_min, float data_max,
+                       float color_min, float color_max);
+  virtual void updateImage(nmb_Image *image);
+
+ protected:
+  nmg_Graphics *d_graphicsDisplay;
+  vrpn_bool d_projectiveTexturesEnabled;
+};
+
+#endif
