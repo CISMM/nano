6257ef488fd083cb598781441d93d79b09aa1486
Administrator <>
2002-07-03 16:26:14 +0000

    2002-07-03  Adam Seeger  <seeger@cs.unc.edu>
            * tip.tcl: added button for sending the tip location as a fiducial
            added controls for tip model

--- src/share/tcl/tip.tcl
+++ src/share/tcl/tip.tcl
@@ -21,10 +21,15 @@ package require Iwidgets
 
 # GLOBAL variables
 # ----------------------------------------------------------------------
-global tip_window_open tip_display_enable tip_display_texture_enable
+global tip_window_open tip_display_enable tip_display_texture_enable \
+       tip_model_mode_image tip_model_mode_conesphere tip_model_mode \
+       tip_cone_sphere_radius tip_cone_sphere_angle tip_cone_sphere_height
 
 set nmInfo(tip) [create_closing_toplevel_with_notify \
                             tip_win tip_window_open]
+set tip_model_mode_image 1
+set tip_model_mode_conesphere 2
+set tip_model_mode 2
 # ------------------------------------------------- end GLOBAL variables
 
 iwidgets::labeledframe $nmInfo(tip).display -labeltext "AFM tip display"
@@ -40,7 +45,48 @@ pack $nmInfo(tip_display).display_texture_checkbutton -anchor w
 # Controls whether or not tip geometry is displayed
 checkbutton $nmInfo(tip_display).display_geometry_checkbutton \
         -text "Display Geometry" -variable tip_display_enable
-pack $nmInfo(tip_display).display_geometry_checkbutton
+pack $nmInfo(tip_display).display_geometry_checkbutton -anchor w
+
+frame $nmInfo(tip_display).tip_model -bd 3 -relief groove
+pack $nmInfo(tip_display).tip_model -side left
+
+label $nmInfo(tip_display).tip_model.frame_label -text "Tip Model"
+pack $nmInfo(tip_display).tip_model.frame_label -side top
+
+frame $nmInfo(tip_display).tip_model.topography_image -bd 3 -relief groove
+pack $nmInfo(tip_display).tip_model.topography_image -side top
+
+radiobutton $nmInfo(tip_display).tip_model.topography_image.button \
+  -variable tip_model_mode \
+  -value 1 -text "use topography image" -justify left
+pack $nmInfo(tip_display).tip_model.topography_image.button -side top
+
+generic_optionmenu \
+   $nmInfo(tip_display).tip_model.topography_image.image_selector \
+   tip_topography_image \
+   "tip topography image" imageNames
+pack $nmInfo(tip_display).tip_model.topography_image.image_selector
+
+frame $nmInfo(tip_display).tip_model.cone_sphere -bd 3 -relief groove
+pack $nmInfo(tip_display).tip_model.cone_sphere -side left -anchor w
+
+radiobutton $nmInfo(tip_display).tip_model.cone_sphere.button \
+  -variable tip_model_mode \
+  -value 2 -text "use cone-sphere" -justify left
+pack $nmInfo(tip_display).tip_model.cone_sphere.button -side top
+
+generic_entry $nmInfo(tip_display).tip_model.cone_sphere.radius \
+  tip_cone_sphere_radius "radius (nm)" numeric
+
+generic_entry $nmInfo(tip_display).tip_model.cone_sphere.angle \
+  tip_cone_sphere_angle "angle (deg)" numeric
+
+generic_entry $nmInfo(tip_display).tip_model.cone_sphere.height \
+  tip_cone_sphere_height "height (nm)" numeric
+
+pack $nmInfo(tip_display).tip_model.cone_sphere.radius \
+     $nmInfo(tip_display).tip_model.cone_sphere.angle \
+     $nmInfo(tip_display).tip_model.cone_sphere.height
 
 # sends current tip location as a fiducial to the alignment function
 button $nmInfo(tip_display).send_fiducial_button \
