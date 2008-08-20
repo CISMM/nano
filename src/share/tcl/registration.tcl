0b99d8f23ccc0dd2626ca9573d9867b539c5a7b1
cquammen <>
2008-08-20 01:00:07 +0000

    2008-08-19  Cory Quammen <cquammen@cs.unc.edu>
    	* registration.tcl: Reorganized controls. Added controls for
    	setting spot tracker parameters.

--- src/share/tcl/registration.tcl
+++ src/share/tcl/registration.tcl
@@ -6,36 +6,69 @@
 #  3rdTech, Inc. 
 #  ===3rdtech===*/
 
-# Not needed.
-#global reg_window_open registration_needed reg_constrain_to_topography
-#global reg_invert_warp
-#global resample_resolution_x resample_resolution_y reg_resample_ratio
-#global reg_surface_comes_from reg_projection_comes_from
-#global resample_image_name
-
 set reg_surface_comes_from "none"
 set reg_projection_comes_from "none"
+
+set spotTrackerFunctions { "None" "Local Max" "Cone" "Disk" "FIONA" "Symmetric" }
 
 # Main dialog, for interaction to create a registered image. 
 set nmInfo(registration) [create_closing_toplevel_with_notify \
                                       registration reg_window_open]
+wm title $nmInfo(registration) "Registration"
 
 ########################
-generic_optionmenu $nmInfo(registration).selection3D \
+
+iwidgets::Labeledframe $nmInfo(registration).topographyControls \
+	-labeltext "Topograpy Image" \
+	-labelpos nw
+set nmInfo(topographyControls) [$nmInfo(registration).topographyControls childsite]
+pack $nmInfo(registration).topographyControls -anchor nw -expand true -fill x
+
+frame $nmInfo(topographyControls).row1
+frame $nmInfo(topographyControls).row2
+frame $nmInfo(topographyControls).row3
+
+pack $nmInfo(topographyControls).row1 $nmInfo(topographyControls).row2 $nmInfo(topographyControls).row3 -anchor nw
+
+generic_optionmenu $nmInfo(topographyControls).row1.selection3D \
         reg_surface_cm(color_comes_from) \
-        "Topography image" imageNames
+        "Image" imageNames
 
-frame $nmInfo(registration).controls3D
-
-button $nmInfo(registration).controls3D.colormap3D -text "Colormap..."  \
+button $nmInfo(topographyControls).row1.colormap3D -text "Colormap..."  \
     -command "show.reg_surf_colorscale"
 
-button $nmInfo(registration).controls3D.refresh -text "Refresh" \
+button $nmInfo(topographyControls).row1.refresh -text "Refresh" \
     -command "set reg_refresh_3D 1"
 
-pack $nmInfo(registration).selection3D $nmInfo(registration).controls3D -anchor nw -pady 3
+pack $nmInfo(topographyControls).row1.selection3D \
+	$nmInfo(topographyControls).row1.colormap3D \
+	$nmInfo(topographyControls).row1.refresh \
+	-anchor nw -side left -pady 1
 
-pack $nmInfo(registration).controls3D.colormap3D $nmInfo(registration).controls3D.refresh -anchor nw -side left -pady 3
+generic_optionmenu $nmInfo(topographyControls).row2.spotTracker \
+    reg_fiducial_spot_tracker_3D "Tracker" spotTrackerFunctions
+
+checkbutton $nmInfo(topographyControls).row2.optimize_radius_checkbutton \
+    -text "Optimize radius" -variable reg_spot_tracker_optimize_radius_3D -anchor nw
+
+pack $nmInfo(topographyControls).row2.spotTracker \
+	$nmInfo(topographyControls).row2.optimize_radius_checkbutton \
+	-anchor w -side left -pady 1
+
+floatscale $nmInfo(topographyControls).row3.radius 0 50 1000 1 1 \
+	reg_spot_tracker_radius_3D "Radius"
+
+floatscale $nmInfo(topographyControls).row3.radius_accuracy 0.001 1 1000 1 1 \
+	reg_spot_tracker_radius_accuracy_3D "Radius accuracy"
+
+floatscale $nmInfo(topographyControls).row3.pixel_accuracy 0.001 1 1000 1 1 \
+	reg_spot_tracker_pixel_accuracy_3D "Pixel accuracy"
+
+pack $nmInfo(topographyControls).row3.radius \
+	$nmInfo(topographyControls).row3.radius_accuracy \
+	$nmInfo(topographyControls).row3.pixel_accuracy \
+	-anchor w -side left -pady 1
+
 
 # Colormap controls, using routines from colormap.tcl
 set nmInfo(reg_surf_colorscale) [create_closing_toplevel reg_surf_colorscale "Registration Topography Color Map" ]
@@ -45,24 +78,69 @@ colormap_controls $nmInfo(reg_surf_colorscale) reg_surface_cm \
         reg_surface_cm(color_comes_from) "Topography image" imageNames
 
 ########################
-generic_optionmenu $nmInfo(registration).selection2D \
+iwidgets::Labeledframe $nmInfo(registration).projectionControls \
+	-labeltext "Projection Image" \
+	-labelpos nw
+set nmInfo(projectionControls) [$nmInfo(registration).projectionControls childsite]
+pack $nmInfo(registration).projectionControls -anchor nw -expand true -fill x
+
+frame $nmInfo(projectionControls).row1
+frame $nmInfo(projectionControls).row2
+frame $nmInfo(projectionControls).row3
+frame $nmInfo(projectionControls).row4
+
+pack $nmInfo(projectionControls).row1 \
+	$nmInfo(projectionControls).row2 \
+	$nmInfo(projectionControls).row3 \
+	$nmInfo(projectionControls).row4 \
+	-anchor nw
+
+generic_optionmenu $nmInfo(projectionControls).row1.selection2D \
         reg_projection_cm(color_comes_from) \
-        "Projection image" imageNames
+        "Image" imageNames
 
-frame $nmInfo(registration).controls2D
-
-button $nmInfo(registration).controls2D.colormap2D -text "Colormap..."  \
+button $nmInfo(projectionControls).row1.colormap2D -text "Colormap..."  \
     -command "show.reg_proj_colorscale"
 
-button $nmInfo(registration).controls2D.refresh -text "Refresh" \
+button $nmInfo(projectionControls).row1.refresh -text "Refresh" \
     -command "set reg_refresh_2D 1"
 
-checkbutton $nmInfo(registration).controls2D.flip_projection_image_checkbutton \
+pack $nmInfo(projectionControls).row1.selection2D \
+	$nmInfo(projectionControls).row1.colormap2D \
+	$nmInfo(projectionControls).row1.refresh \
+	-anchor nw -side left -pady 1
+
+
+checkbutton $nmInfo(projectionControls).row2.flip_projection_image_x_checkbutton \
     -text "Flip image in X" -variable reg_proj_flipX -anchor nw
 
-pack $nmInfo(registration).selection2D $nmInfo(registration).controls2D -anchor nw -pady 3
+pack $nmInfo(projectionControls).row2.flip_projection_image_x_checkbutton \
+	-anchor w -side left -pady 1
 
-pack $nmInfo(registration).controls2D.colormap2D $nmInfo(registration).controls2D.refresh $nmInfo(registration).controls2D.flip_projection_image_checkbutton -anchor nw -side left -pady 3
+generic_optionmenu $nmInfo(projectionControls).row3.spotTracker \
+    reg_fiducial_spot_tracker_2D "Tracker" spotTrackerFunctions
+
+checkbutton $nmInfo(projectionControls).row3.optimize_radius_checkbutton \
+    -text "Optimize radius" -variable reg_spot_tracker_optimize_radius_2D -anchor nw
+
+pack $nmInfo(projectionControls).row3.spotTracker \
+	$nmInfo(projectionControls).row3.optimize_radius_checkbutton \
+	-anchor w -side left -pady 1
+
+floatscale $nmInfo(projectionControls).row4.radius 0 50 1000 1 1 \
+	reg_spot_tracker_radius_2D "Radius"
+
+floatscale $nmInfo(projectionControls).row4.radius_accuracy 0.001 1 1000 1 1 \
+	reg_spot_tracker_radius_accuracy_2D "Radius accuracy"
+
+floatscale $nmInfo(projectionControls).row4.pixel_accuracy 0.001 1 1000 1 1 \
+	reg_spot_tracker_pixel_accuracy_2D "Pixel accuracy"
+
+pack $nmInfo(projectionControls).row4.radius \
+	$nmInfo(projectionControls).row4.radius_accuracy \
+	$nmInfo(projectionControls).row4.pixel_accuracy \
+	-anchor w -side left -pady 1
+
 
 # Colormap controls, using routines from colormap.tcl
 set nmInfo(reg_proj_colorscale) [create_closing_toplevel reg_proj_colorscale "Registration Projection Color Map" ]
@@ -88,94 +166,137 @@ trace variable reg_window_open w reg_close_cmap_windows
 set auto_align_resolution_list {none}
 set auto_align_mode_list {none}
 
-button $nmInfo(registration).auto_align -text "Auto Align" -command \
+iwidgets::Labeledframe $nmInfo(registration).auto_align \
+	-labeltext "Automatic Alignment" \
+	-labelpos nw
+set nmInfo(auto_align) [$nmInfo(registration).auto_align childsite]
+pack $nmInfo(registration).auto_align -anchor nw -expand true -fill x
+
+frame $nmInfo(auto_align).row1
+frame $nmInfo(auto_align).row2
+
+pack $nmInfo(auto_align).row1 $nmInfo(auto_align).row2 -anchor nw
+
+button $nmInfo(auto_align).row1.auto_align -text "Auto Align" -command \
     { set auto_align_requested 1 }
-pack $nmInfo(registration).auto_align -anchor nw
 
-generic_entry $nmInfo(registration).num_iteration_entry \
+generic_entry $nmInfo(auto_align).row1.num_iteration_entry \
         auto_align_num_iterations "# iterations" numeric
-pack $nmInfo(registration).num_iteration_entry -anchor nw
 
-generic_entry $nmInfo(registration).step_size_entry auto_align_step_size \
-        "step size (0,1)" real
-pack $nmInfo(registration).step_size_entry -anchor nw
+generic_entry $nmInfo(auto_align).row1.step_size_entry auto_align_step_size \
+        "Step size (0,1)" real
 
-generic_optionmenu $nmInfo(registration).resolution_selector \
+pack $nmInfo(auto_align).row1.auto_align \
+	$nmInfo(auto_align).row1.num_iteration_entry \
+	$nmInfo(auto_align).row1.step_size_entry \
+	-anchor w -side left -pady 1
+
+generic_optionmenu $nmInfo(auto_align).row2.resolution_selector \
         auto_align_resolution \
-        "resolution level" auto_align_resolution_list
-pack $nmInfo(registration).resolution_selector -anchor nw
+        "Resolution level" auto_align_resolution_list
 
-generic_optionmenu $nmInfo(registration).mode_selector \
+generic_optionmenu $nmInfo(auto_align).row2.mode_selector \
         auto_align_mode \
-        "mode" auto_align_mode_list
-pack $nmInfo(registration).mode_selector -anchor nw
+        "Mode" auto_align_mode_list
+
+pack $nmInfo(auto_align).row2.resolution_selector \
+	$nmInfo(auto_align).row2.mode_selector \
+	-anchor w -side left
 
 ################ end of controls for automatic alignment
 
 ################ some manually-adjustable transformation parameters #####
 
-frame $nmInfo(registration).transformParameters -bd 3 -relief groove
-pack $nmInfo(registration).transformParameters -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.scaleX \
-#        reg_scaleX "scale X" real
-#pack $nmInfo(registration).transformParameters.scaleX -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.scaleY \
-#        reg_scaleY "scale Y" real
-#pack $nmInfo(registration).transformParameters.scaleY -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.translateX \
-#        reg_translateX "translate X" real
-#pack $nmInfo(registration).transformParameters.translateX -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.translateY \
-#        reg_translateY "translate Y" real
-#pack $nmInfo(registration).transformParameters.translateY -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.translateZ \
-#        reg_translateZ "translate Z" real
-#pack $nmInfo(registration).transformParameters.translateZ -anchor nw
-
-#generic_entry $nmInfo(registration).transformParameters.rotate2D_Z \
-#        reg_rotate2D_Z "rotate Z (2D)" real
-#pack $nmInfo(registration).transformParameters.rotate2D_Z -anchor nw
-
-generic_entry $nmInfo(registration).transformParameters.rotate3D_X \
-        reg_rotate3D_X "altitude angle (degrees)" real
-pack $nmInfo(registration).transformParameters.rotate3D_X -anchor nw
-
-generic_entry $nmInfo(registration).transformParameters.rotate3D_Z \
-        reg_rotate3D_Z "azimuth angle (degrees)" real
-pack $nmInfo(registration).transformParameters.rotate3D_Z -anchor nw
+iwidgets::Labeledframe $nmInfo(registration).transformParameters \
+	-labeltext "Manual Alignment Parameters" \
+	-labelpos nw
+set nmInfo(transformParameters) [$nmInfo(registration).transformParameters childsite]
+#pack $nmInfo(registration).transformParameters -anchor nw -expand true -fill x
 
 #generic_entry $nmInfo(registration).transformParameters.shearZ \
 #        reg_shearZ "shear Z" real
 #pack $nmInfo(registration).transformParameters.shearZ -anchor nw
 
+frame $nmInfo(transformParameters).row1
+frame $nmInfo(transformParameters).row2
+frame $nmInfo(transformParameters).row3
+pack $nmInfo(transformParameters).row1 \
+	$nmInfo(transformParameters).row2 \
+	$nmInfo(transformParameters).row3 \
+	-anchor nw -side top
+
+generic_entry $nmInfo(transformParameters).row1.translateX \
+	reg_translateX "Translate X" real
+
+generic_entry $nmInfo(transformParameters).row1.translateY \
+	reg_translateY "Translate Y" real
+
+#generic_entry $nmInfo(registration).transformParameters.translateZ \
+#        reg_translateZ "translate Z" real
+#pack $nmInfo(registration).transformParameters.translateZ -anchor nw
+
+generic_entry $nmInfo(transformParameters).row1.scaleX \
+	reg_scaleX "Scale X" real
+
+generic_entry $nmInfo(transformParameters).row1.scaleY \
+	reg_scaleY "Scale Y" real
+
+
+#pack $nmInfo(transformParameters).row1.translateX \
+#	$nmInfo(transformParameters).row1.translateY \
+#	$nmInfo(transformParameters).row1.scaleX \
+#	$nmInfo(transformParameters).row1.scaleY \
+#	-anchor nw -side left
+
+generic_entry $nmInfo(transformParameters).row2.rotate2D_Z \
+	reg_rotate2D_Z "2D rotation" real
+
+#pack $nmInfo(transformParameters).row2.rotate2D_Z \
+#	-anchor nw -side left
+
+generic_entry $nmInfo(transformParameters).row3.rotate3D_X \
+        reg_rotate3D_X "Altitude angle (deg.)" real
+
+generic_entry $nmInfo(transformParameters).row3.rotate3D_Z \
+        reg_rotate3D_Z "Azimuth angle (deg.)" real
+
+#pack $nmInfo(transformParameters).row3.rotate3D_X \
+#	$nmInfo(transformParameters).row3.rotate3D_Z \
+#	-anchor nw -side left
+
+
 #################################################################
 set reg_transformation_source_list {none}
 
-frame $nmInfo(registration).texture -bd 3 -relief groove
-pack $nmInfo(registration).texture -anchor nw
+iwidgets::Labeledframe $nmInfo(registration).texture \
+	-labeltext "Image Overlay" \
+	-labelpos nw
+set nmInfo(texture_controls) [$nmInfo(registration).texture childsite]
+pack $nmInfo(registration).texture -anchor nw -expand true -fill x
 
-generic_optionmenu $nmInfo(registration).texture.source_selector \
+frame $nmInfo(texture_controls).row1
+frame $nmInfo(texture_controls).row2
+pack $nmInfo(texture_controls).row1 $nmInfo(texture_controls).row2 -anchor nw
+
+generic_optionmenu $nmInfo(texture_controls).row1.source_selector \
         reg_transformation_source \
-        "source" reg_transformation_source_list
-pack $nmInfo(registration).texture.source_selector -anchor nw
+        "Source" reg_transformation_source_list
 
-checkbutton $nmInfo(registration).texture.display_texture \
-    -text "Display in Surface View" -variable reg_display_texture -anchor nw
+floatscale $nmInfo(texture_controls).row1.texture_alpha 0 1 1000 1 1 \
+    reg_texture_alpha "Texture alpha value"
 
-pack $nmInfo(registration).texture.display_texture -anchor nw
+checkbutton $nmInfo(texture_controls).row1.display_texture \
+    -text "Display" -variable reg_display_texture -anchor nw
 
-floatscale $nmInfo(registration).texture.texture_alpha 0 1 1000 1 1 \
-    reg_texture_alpha "Texture Alpha Value"
+pack $nmInfo(texture_controls).row1.source_selector \
+	$nmInfo(texture_controls).row1.texture_alpha \
+	-side left -pady 1 -fill x
 
-pack $nmInfo(registration).texture.texture_alpha -anchor nw
+pack $nmInfo(texture_controls).row1.display_texture -side left -pady 1 -fill x
+
 
 iwidgets::Labeledframe $nmInfo(registration).rsplane \
-	-labeltext "Create Plane (match Topog image region)" \
+	-labeltext "Create Plane (match topography image region)" \
 	-labelpos nw
 set nmInfo(reg_plane) [$nmInfo(registration).rsplane childsite]
 pack $nmInfo(registration).rsplane -fill x -anchor nw
@@ -203,13 +324,16 @@ generic_radiobox $nmInfo(reg_image).constrain_to_topography \
 	reg_constrain_to_topography  \
 	"Extent of image includes..." { "Whole projection image" \
         "Topography region only" }
-pack $nmInfo(reg_image).constrain_to_topography -fill x -anchor nw
+$nmInfo(reg_image).constrain_to_topography configure -orient horizontal
+
+pack $nmInfo(reg_image).constrain_to_topography -anchor nw
 
 generic_radiobox $nmInfo(reg_image).invert_warp \
 	reg_invert_warp  \
-	"When Resampling..." { "Warp Projection" "Warp Topography" }
+	"When resampling..." { "Warp projection" "Warp topography" }
+$nmInfo(reg_image).invert_warp configure -orient horizontal
 
-pack $nmInfo(reg_image).invert_warp -fill x -anchor nw
+pack $nmInfo(reg_image).invert_warp -anchor nw
 
 floatscale $nmInfo(reg_image).resample_ratio 0 1 101 1 1 \
        reg_resample_ratio "Portion unwarped image intensity mixed"
@@ -219,15 +343,19 @@ pack $nmInfo(reg_image).resample_ratio -anchor nw
 # stuff for creating resampled data:
 set resample_image_name ""
 
-generic_entry $nmInfo(reg_image).resolution_x \
-        resample_resolution_x "x resolution (10, 10000)" numeric
+frame $nmInfo(reg_image).row1
+frame $nmInfo(reg_image).row2
+pack $nmInfo(reg_image).row1 $nmInfo(reg_image).row2 -anchor nw -expand true -fill x
 
-generic_entry $nmInfo(reg_image).resolution_y \
-		resample_resolution_y "y resolution (10, 10000)" numeric
+generic_entry $nmInfo(reg_image).row1.resolution_x \
+        resample_resolution_x "X resolution (10, 10000)" numeric
 
-pack $nmInfo(reg_image).resolution_x $nmInfo(reg_image).resolution_y \
-	 -fill x -anchor nw
+generic_entry $nmInfo(reg_image).row1.resolution_y \
+		resample_resolution_y "Y resolution (10, 10000)" numeric
 
-generic_entry $nmInfo(reg_image).resample_image resample_image_name \
+pack $nmInfo(reg_image).row1.resolution_x $nmInfo(reg_image).row1.resolution_y \
+	 -fill x -anchor nw -side left
+
+generic_entry $nmInfo(reg_image).row2.resample_image resample_image_name \
 	"Resample image name" ""
-pack $nmInfo(reg_image).resample_image  -fill x -anchor nw
+pack $nmInfo(reg_image).row2.resample_image  -fill x -anchor nw
