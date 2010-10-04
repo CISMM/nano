351e6153fbd00257795c0de806b83260a35023e1
cakici <>
2010-10-04 22:58:17 +0000

    * nano_debug.bat: new batch file for debug mode * nano_release.bat: new batch file for release mode

--- src/nano_release.bat
+++ src/nano_release.bat
@@ -1,6 +1,6 @@
 set TCL_LIBRARY=C:\nsrg\external\pc_win32\tcl\lib\tcl8.3
-set NM_TCL_DIR=D:\cakici\RA\nano\src\share\tcl
-set NANO_ROOT=D:\cakici\RA\nano\src\
-set NM_COLORMAP_DIR=D:\cakici\RA\nano\src\share\colormaps
+set NM_TCL_DIR=G:\RA\nano\src\share\tcl
+set NANO_ROOT=G:\RA\nano\src\
+set NM_COLORMAP_DIR=G:\RA\nano\src\share\colormaps
 set PATH=C:\nsrg\external\pc_win32\qt\bin\;C:\Program Files\NanoManipulator\bin\;%PATH%
-D:\cakici\RA\nano\obj\pc_win32\release\app\nano\nano.exe
+G:\RA\nano\obj\pc_win32\release\app\nano\nano.exe
